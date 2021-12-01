a2280912da574b668cc38b038f985dbb9ec9a292
James Wood <55083238+Jamzak@users.noreply.github.com>
2021-12-02 00:03:49 +0100

    speeed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,9 @@
-
+import kotlin.system.measureNanoTime
 
 fun main() {
-    println(Task1Day1().readFile("src/main/resources/input.txt"))
-    println(Task2Day1().readFile("src/main/resources/input.txt"))
+    val time = measureNanoTime {
+        println(Task1Day1().readFile("src/main/resources/input.txt"))
+        println(Task2Day1().readFile("src/main/resources/input.txt"))
+    }
+    println(time / 1_000_000)
 }
