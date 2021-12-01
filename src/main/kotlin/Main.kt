f325515eb3420648c464bb5b31d0a99ab2dfcf1e
James Wood <55083238+Jamzak@users.noreply.github.com>
2021-12-01 23:08:39 +0100

    change input path

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,10 @@
 fun main(args: Array<String>) {
 
     val firstTask = FirstTask()
-    firstTask.readFile("src/main/kotlin/1dec/input.txt")
+    firstTask.readFile("src/main/resources/input.txt")
     println(firstTask.counter)
 
     val secondTask = SecondTask()
-    secondTask.readFile("src/main/kotlin/1dec/input.txt")
+    secondTask.readFile("src/main/resources/input.txt")
     println(secondTask.counter)
 }
